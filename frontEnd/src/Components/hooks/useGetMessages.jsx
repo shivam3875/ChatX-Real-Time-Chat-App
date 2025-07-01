988002ddd186fd5d6b7bf86c8fb7322615f524c1
shivam3875 <kumarvikas10015@gmail.com>
2025-07-02 00:36:29 +0530

    comment removed

--- frontEnd/src/Components/hooks/useGetMessages.jsx
+++ frontEnd/src/Components/hooks/useGetMessages.jsx
@@ -2,34 +2,6 @@ import { useState } from 'react'
 import { useConversationsContext } from '../Context/ConversationContext';
 import toast from "react-hot-toast"
 
-// const useGetMessages = () => {
-// 	const [loading, setLoading] = useState(false);
-//     const {setselectedconvo}=useConversationsContext();
-// 	const { messages, setMessages} = useMessagesContext();
-
-// 	useEffect(() => {
-// 		const getMessages = async () => {
-// 			setLoading(true);
-// 			try {
-// 				const res = await fetch(`/api/messages/${selectedConversation._id}`);
-// 				const data = await res.json();
-// 				if (data.error) throw new Error(data.error);
-// 				setMessages(data);
-// 			} catch (error) {
-// 				toast.error(error.message);
-// 			} finally {
-// 				setLoading(false);
-// 			}
-// 		};
-
-// 		if (selectedConversation?._id) getMessages();
-// 	}, [selectedConversation?._id, setMessages]);
-
-// 	return { messages, loading };
-// };
-// export default useGetMessages;
-
-
 const useGetMessages = () => {
     const [loading,setloading] = useState(false);
     const {setselectedconvo}=useConversationsContext();
